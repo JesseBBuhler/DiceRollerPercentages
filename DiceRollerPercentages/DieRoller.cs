a129aa316d2d1538f9f15708394cd782cba25fce
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-01-17 18:54:51 -0700

    Submission.

--- DiceRollerPercentages/DieRoller.cs
+++ DiceRollerPercentages/DieRoller.cs
@@ -6,11 +6,36 @@ namespace DiceRollerPercentages
 {
     class DieRoller
     {
-        public void RollDie()
+        private int[] rollLog = new int[11];
+        private int numRolls = 0;
+
+        public void rollDice(int inNumRolls)
         {
-            Console.WriteLine("What number do you want to roll?");
-            string roll = Console.ReadLine();
-            Console.WriteLine("I rolled a " + roll);
+            numRolls = inNumRolls;
+            Random rnd = new Random();
+            for (int i = 0; i < numRolls; i++)
+            {
+                int roll1 = rnd.Next(6) + 1;
+                int roll2 = rnd.Next(6) + 1;
+                int total = roll1 + roll2;
+                rollLog[total - 2] = rollLog[total - 2] + 1;
+            }
+
+
+        }
+
+        public void histogram()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                string starString = "";
+                int percentage = (rollLog[i] * 100)/ numRolls;
+                for (int j = 0; j < percentage; j++)
+                {
+                    starString = starString + "*";
+                }
+                Console.WriteLine((i + 2) + ": " + starString);
+            }
         }
     }
 }
--- DiceRollerPercentages/Program.cs
+++ DiceRollerPercentages/Program.cs
@@ -6,12 +6,24 @@ namespace DiceRollerPercentages
     {
         public static void Main(string[] args)
         {
-            int[] rolls = [1, 2, 3]
+            int numRolls = 0;
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.WriteLine("How many dice rolls would you like to simulate? ");
+            if (int.TryParse(Console.ReadLine(), out numRolls))
+            {
+                Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+                Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+                Console.WriteLine("Total number of rolls = " + numRolls);
+                DieRoller DR = new DieRoller();
+                DR.rollDice(numRolls);
+                DR.histogram();
+                Console.WriteLine("Thank you for using the dice throwing simulator.  Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Input was invalid. Please restart program.");
+            }
         }
 
-        private static void PrintHistogram (int[] rolls)
-        {
-            Console.WriteLine("Hello World");
-        }
     }
 }
