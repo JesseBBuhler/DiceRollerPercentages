b5e6ea7e9a1bc9599e993eb14d2166602d93b54f
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-01-17 17:51:09 -0700

    Add project files.

---
+++ DiceRollerPercentages/DieRoller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRollerPercentages
+{
+    class DieRoller
+    {
+        public void RollDie()
+        {
+            Console.WriteLine("What number do you want to roll?");
+            string roll = Console.ReadLine();
+            Console.WriteLine("I rolled a " + roll);
+        }
+    }
+}
---
+++ DiceRollerPercentages/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiceRollerPercentages
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] rolls = [1, 2, 3]
+        }
+
+        private static void PrintHistogram (int[] rolls)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
